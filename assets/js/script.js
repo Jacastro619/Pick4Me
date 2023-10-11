1868c7083202186939547b3b128670b00f5e38b3
Jacastro619 <jorgecastro619@gmail.com>
2023-10-10 21:35:39 -0700

    Add yelp API urls for data

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,3 +7,45 @@ window.onload = function () {
     zoom: 4,
   });
 };
+
+var options = {
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer cpIZVMVOeEQWNK3YQtqsptY8MgVpErO4syU6mpNzUmDE26gJwG34Z6GVQHXpl-mg_v0ZneUmCS3qtJPVNB6n9r-lXzl9CsVAtIgyiqW-Wkb5z9ZAQRtrrq_qE_khZXYx",
+  },
+};
+
+var searchUrl =
+  "https://corsproxy.io/?https://api.yelp.com/v3/businesses/search?location=93720&term=thia&radius=16000&limit=10";
+
+fetch(searchUrl, options)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (data) {
+    console.log(data);
+    console.log(data.businesses[0].name);
+    console.log(
+      `Address: ${data.businesses[0].location.display_address.join(", ")}`
+    );
+    console.log(`Price: ${data.businesses[0].price}`);
+    console.log(`This restaurant has ${data.businesses[0].rating} stars`);
+
+    var reviewUrl = `https://corsproxy.io/?https://api.yelp.com/v3/businesses/${data.businesses[0].id}/reviews?sort_by=newest`;
+
+    fetch(reviewUrl, options)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        console.log("3 most recent star ratings");
+        for (var i = 0; i < data.reviews.length; i++) {
+          var ratingNum = i + 1;
+          console.log(
+            `Rating number ${ratingNum}: ${data.reviews[i].rating} stars by ${data.reviews[i].user.name}`
+          );
+        }
+      });
+  });
