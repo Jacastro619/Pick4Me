cb595d5b57c83619899d22245b9676eeec947f4d
Jacastro619 <jorgecastro619@gmail.com>
2023-10-11 17:04:32 -0700

    added a function to get input values and call the api based on values

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,6 +16,14 @@ var homeMsg = $("#home-msg");
 var inputAlert = $(".custom-message");
 var closeAlertbtn = $(".close-alert");
 
+var options = {
+  headers: {
+    accept: "application/json",
+    Authorization:
+      "Bearer cpIZVMVOeEQWNK3YQtqsptY8MgVpErO4syU6mpNzUmDE26gJwG34Z6GVQHXpl-mg_v0ZneUmCS3qtJPVNB6n9r-lXzl9CsVAtIgyiqW-Wkb5z9ZAQRtrrq_qE_khZXYx",
+  },
+};
+
 function closeAlert() {
   $(inputAlert).addClass("hidden");
 }
@@ -29,73 +37,73 @@ function showUserInput() {
   var addressUser = localStorage.getItem("userAddress");
   var radiusUser = localStorage.getItem("userRadius");
   var foodUser = localStorage.getItem("userFood");
-  if (!addressUser || !radiusUser || foodUser) {
+  if (!addressUser || !radiusUser || !foodUser) {
     return;
   }
 }
 
-function handleHomeForm(event) {
+function handleHomeForm() {
   console.log("clicked - Saving home data to local storage");
-  //event.preventDefault();
 
-  var userAddress = $('input[name="one-input-address"]').val();
-  var userRadius = $('input[name="one-input-radius"]').val();
-  var userFood = $('input[name="one-input-food"]').val();
+  var userAddress = $("#one-input-address").val();
+  var userRadius = $("#one-input-radius").val();
+  var userFood = $("#one-input-food").val();
 
   if (!userAddress || !userRadius || !userFood) {
     inputAlert.removeClass("hidden");
   } else {
     homePageEl.addClass("hidden");
     resultsPageEl.removeClass("hidden");
+    localStorage.setItem("userAddress", userAddress);
+    localStorage.setItem("userRadius", userRadius);
+    localStorage.setItem("UserFood", userFood);
+    fetchSearch();
+    userAddress = $("#one-input-address").val("");
+    userRadius = $("#one-input-radius").val("");
+    userFood = $("#one-input-food").val("");
   }
-
-  localStorage.setItem("userAddress", userAddress);
-  localStorage.setItem("userRadius", userRadius);
-  localStorage.setItem("UserFood", userFood);
-  showUserInput();
 }
 
-// var options = {
-//   headers: {
-//     accept: "application/json",
-//     Authorization:
-//       "Bearer cpIZVMVOeEQWNK3YQtqsptY8MgVpErO4syU6mpNzUmDE26gJwG34Z6GVQHXpl-mg_v0ZneUmCS3qtJPVNB6n9r-lXzl9CsVAtIgyiqW-Wkb5z9ZAQRtrrq_qE_khZXYx",
-//   },
-// };
+function fetchSearch() {
+  var userAddress = $("#one-input-address").val();
+  var userRadius = $("#one-input-radius").val();
+  var userFood = $("#one-input-food").val();
 
-// var searchUrl =
-//   "https://corsproxy.io/?https://api.yelp.com/v3/businesses/search?location=93720&term=thia&radius=16000&limit=10";
+  var searchUrl = `https://corsproxy.io/?https://api.yelp.com/v3/businesses/search?location=${userAddress}&term=${userFood}&radius=${
+    userRadius * 1609
+  }&limit=10`;
 
-// fetch(searchUrl, options)
-//   .then(function (response) {
-//     return response.json();
-//   })
-//   .then(function (data) {
-//     console.log(data);
-//     console.log(data.businesses[0].name);
-//     console.log(
-//       `Address: ${data.businesses[0].location.display_address.join(", ")}`
-//     );
-//     console.log(`Price: ${data.businesses[0].price}`);
-//     console.log(`This restaurant has ${data.businesses[0].rating} stars`);
+  fetch(searchUrl, options)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      console.log(data.businesses[0].name);
+      console.log(
+        `Address: ${data.businesses[0].location.display_address.join(", ")}`
+      );
+      console.log(`Price: ${data.businesses[0].price}`);
+      console.log(`This restaurant has ${data.businesses[0].rating} stars`);
 
-//     var reviewUrl = `https://corsproxy.io/?https://api.yelp.com/v3/businesses/${data.businesses[0].id}/reviews?sort_by=newest`;
+      var reviewUrl = `https://corsproxy.io/?https://api.yelp.com/v3/businesses/${data.businesses[0].id}/reviews?sort_by=newest`;
 
-//     fetch(reviewUrl, options)
-//       .then(function (response) {
-//         return response.json();
-//       })
-//       .then(function (data) {
-//         console.log(data);
-//         console.log("3 most recent star ratings");
-//         for (var i = 0; i < data.reviews.length; i++) {
-//           var ratingNum = i + 1;
-//           console.log(
-//             `Rating number ${ratingNum}: ${data.reviews[i].rating} stars by ${data.reviews[i].user.name}`
-//           );
-//         }
-//       });
-//   });
+      fetch(reviewUrl, options)
+        .then(function (response) {
+          return response.json();
+        })
+        .then(function (data) {
+          console.log(data);
+          console.log("3 most recent star ratings");
+          for (var i = 0; i < data.reviews.length; i++) {
+            var ratingNum = i + 1;
+            console.log(
+              `Rating number ${ratingNum}: ${data.reviews[i].rating} stars by ${data.reviews[i].user.name}`
+            );
+          }
+        });
+    });
+}
 
 function toResultPage() {
   console.log("works");
